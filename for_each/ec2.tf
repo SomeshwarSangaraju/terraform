8acc1eabe038b3590280cbac9e666e475840f0d0
SSR <someshwar.sangaraju19@gmail.com>
2025-10-15 20:02:12 +0530

    for each

--- for_each/ec2.tf
+++ for_each/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "terraform" {
-  for_each = length(toset(var.ec2_instance))
+  for_each = toset(var.ec2_instance)
   ami = "ami-09c813fb71547fc4f"
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_all.id]

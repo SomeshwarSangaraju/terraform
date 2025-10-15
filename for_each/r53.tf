359c6b497c895d7f0b1f778bc82d6a4cf51049f2
SSR <someshwar.sangaraju19@gmail.com>
2025-10-15 20:09:59 +0530

    terraform

--- for_each/r53.tf
+++ for_each/r53.tf
@@ -1,9 +1,9 @@
 # resource "aws_route53_record" "roboshop" {
 #   for_each = aws_instance.terraform
 #   zone_id = var.zone_id
-#   name    = "${var.ec2_instance[each.value]}" == "frontend" ? "${var.domain_name}": "${var.ec2_instance[each.value]}.${var.domain_name}"
+#   name    = each.key == "frontend" ? "${var.domain_name}": [each.value]."${var.domain_name}"
 #   type    = "A"
 #   ttl     = 1
-#   records =   "${var.ec2_instance[each.value]}" == "frontend" ? [aws_instance.terraform[each.value].public_ip] :  [aws_instance.terraform[each.value].private_ip]
+#   records =   each.key == "frontend" ? [each.value.public_ip] :  [each.value.private_ip]
 #   allow_overwrite = true
 # }
